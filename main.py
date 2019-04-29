a2e6960516342e393ffb655c52929e62033bb9ed
verozhang <veromestesso@gmail.com>
2019-04-29 17:54:52 -0400

    More adjustments. This version does not work.

--- main.py
+++ main.py
@@ -86,14 +86,13 @@ def conv_consonance_rate(signal, sampling_rate):
     for i in range(len(chroma[0])):
         cur_slice = chroma[:, i]
         cur_slice_c = []
-        cur_sum = 0
         for j in range(12):
+            cur_sum = 0
             for k in range(12):
                 cur_sum += cur_slice[j][(j+k) % 12]
+            cur_slice_c.append(cur_sum)
     return
 # conv_consonance_rate
-
-
 
 
 def two_beat_detect(signal, sampling_rate, hop_length):
@@ -136,76 +135,102 @@ def four_beat_detect(signal, sampling_rate, hop_length):
 # four_beat_detect
 
 
-if __name__ == '__main__':
-    #y, sr = lr.load('3.mp3')
-    '''
-    sl = slice_by_beat(y, sr, 512)
+def pattern_beat_detect(signal, sampling_rate, hop_length, beat):
+    slices = slice_by_beat(signal, sampling_rate, hop_length)
     var = []
-    vard = []
-    for i in range(len(sl)):
-        var.append(np.var(sl[i]))
-    for i in range(len(var) - 2):
-        vard.append(2 * var[i+1] - var[i] - var[i+2])
-    plt.plot(vard)
-    plt.show()
-    #rate = diff_consonance_rate(y, sr)
-    cr = consonance_rate(y, sr)
-    dcr = diff_consonance_rate(y, sr)
-    rate2 = two_beat_detect(y, sr, 512)
-    rate3 = three_beat_detect(y, sr, 512)
-    rate4 = four_beat_detect(y, sr, 512)
-    print(cr, dcr, rate2, rate3, rate4)'''
+    for item in slices:
+        var.append(np.var(item))
+    counter = 0
+    for i in range(len(var) - 2 * beat):
+        cur = []    # current pattern
+        nxt = []    # next pattern
+        for j in range(beat):
+            cur.append(var[i+j] < var[i+j+1])
+            nxt.append(var[i+beat+j] < var[i+beat+j+1])
+        if cur == nxt:
+            counter += 1
+    return counter / len(var)
+# pattern_beat_detect
+
+
+if __name__ == '__main__':
+    
     data = []
     labels = []
     testdata = []
-    #testlabels = []
 
     for i in range(10):
         filename = str(i) + '.mp3'
         y, sr = lr.load(filename)
 
-        slr = np.random.randint(len(y) - 1323000)
-        sl = y[slr:slr+1323000]
-        chroma = lr.feature.chroma_stft(sl, sr)
-        data.append(chroma)
+        for j in range(10):
+            slr = np.random.randint(len(y) - 1323000)
+            sl = y[slr:slr + 1323000]
+            sl = np.reshape(sl, (1323, 1000))
+            data.append(sl)
+            labels.append(0)
 
         slr = np.random.randint(len(y) - 1323000)
         sl = y[slr:slr + 1323000]
-        chroma = lr.feature.chroma_stft(sl, sr)
-        testdata.append(chroma)
+        sl = np.reshape(sl, (1323, 1000))
+        testdata.append(sl)
 
-        labels.append(0)
         print('File ' + filename + ' read successfully.')
 
     for i in range(10):
         filename = str(i + 10) + '.mp3'
         y, sr = lr.load(filename)
 
-        slr = np.random.randint(len(y) - 1323000)
-        sl = y[slr:slr + 1323000]
-        chroma = lr.feature.chroma_stft(sl, sr)
-        data.append(chroma)
+        for j in range(10):
+            slr = np.random.randint(len(y) - 1323000)
+            sl = y[slr:slr + 1323000]
+            sl = np.reshape(sl, (1323, 1000))
+            data.append(sl)
+            labels.append(1)
 
         slr = np.random.randint(len(y) - 1323000)
         sl = y[slr:slr + 1323000]
-        chroma = lr.feature.chroma_stft(sl, sr)
-        testdata.append(chroma)
+        sl = np.reshape(sl, (1323, 1000))
+        testdata.append(sl)
 
-        labels.append(1)
         print('File ' + filename + ' read successfully.')
 
     data = np.array(data)
     testdata = np.array(testdata)
 
-    model = keras.Sequential()
-    model.add(keras.layers.LSTM(units=32, input_shape=(12, 2584), return_sequences=True))
-    model.add(keras.layers.Dropout(rate=0.2))
-    model.add(keras.layers.LSTM(units=32))
-    model.add(keras.layers.Dropout(rate=0.2))
-    model.add(keras.layers.Dense(units=1))
-    model.add(keras.layers.Activation(activation='sigmoid'))
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    model.summary()
-    model.fit(x=data, y=labels, epochs=10000, batch_size=32)
-    testlabels = model.predict(x=testdata)
-    print(testlabels)
+    model0 = keras.Sequential()
+    model0.add(keras.layers.LSTM(units=128, input_shape=(1323, 1000), return_sequences=True))
+    model0.add(keras.layers.Dropout(rate=0.2))
+    model0.add(keras.layers.LSTM(units=32))
+    model0.add(keras.layers.Dropout(rate=0.2))
+    model0.add(keras.layers.Dense(units=1))
+    model0.add(keras.layers.Activation(activation='sigmoid'))
+    model0.compile(optimizer='adam', loss='mean_squared_error')
+    model0.summary()
+    model0.fit(x=data, y=labels, epochs=100, batch_size=100)
+
+    testlabels = model0.predict(x=testdata)
+
+    data1 = []
+    testdata1 = []
+
+    for i in range(len(data)):
+        new_item = []
+        new_item.append(testlabels[i])
+        new_item.append(consonance_rate(data[i], 22050))
+        data1.append(new_item)
+
+    model1 = keras.Sequential()
+    model1.add(keras.layers.Dense(units=32, input_shape=(2, )))
+    model1.add(keras.layers.Activation(activation='sigmoid'))
+    model1.add(keras.layers.Dense(units=1))
+    model1.add(keras.layers.Activation(activation='sigmoid'))
+    model1.compile(optimizer='adam', loss='mean_squared_error')
+    model1.summary()
+    model1.fit(x=data1, y=labels, epochs=100, batch_size=100)
+
+    selftest1 = model1.predict(x=data)
+    testlabels1 = model1.predict(x=testdata)
+
+    print(selftest1)
+    print(testlabels1)
